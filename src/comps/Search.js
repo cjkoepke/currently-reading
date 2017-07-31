efa599ea316e2422c234b8018f746ed4c4135bc4
Calvin Koepke <hello@calvinkoepke.com>
2017-07-31 08:09:44 -0600

    feat: add autosuggestions to search input.

--- src/comps/Search.js
+++ src/comps/Search.js
@@ -4,7 +4,17 @@ import BookList from './BookList';
 import * as api from '../utils/BooksAPI';
 import { Link } from 'react-router-dom';
 import _debounce from 'debounce';
-// import sortBy from 'sort-by';
+
+const searchTerms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'History', 'History', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Program Javascript', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'];
+
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : searchTerms.filter(word =>
+    word.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
 
 class Search extends Component {
     constructor(props) {
@@ -12,7 +22,8 @@ class Search extends Component {
         this.state = {
             query: '',
             books: [],
-            error: false
+            error: false,
+            suggestions: []
         }
     }
 
@@ -44,31 +55,14 @@ class Search extends Component {
 
         // Immediately update input state.
         const query = e.target.value;
-        this.setState({query});
+        this.setState({
+            query: query,
+            suggestions: getSuggestions(query)
+        });
 
         // Debounce the BookList update to avoid overload.
         query &&
-            _debounce(
-                api.search(query)
-                    .then(books => {
-                        if (!books.length) {
-                            throw new Error( `The returned query was not an array: ${books.error}` );
-                        }
-
-                        // Validate response against book shelf data.
-                        return this.validateBooks(books);
-                    })
-                    .then(validated => {
-                        this.setState({
-                            books: validated,
-                            error: false
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        this.setState({error: true});
-                    })
-            , 500)
+            this.updateResults(query);
 
         // Reset books if search query is empty.
         !query &&
@@ -78,11 +72,48 @@ class Search extends Component {
             });
     }
 
+    updateResults = (query) => {
+        _debounce(
+            api.search(query)
+                .then(books => {
+                    if (!books.length) {
+                        throw new Error( `The returned query was not an array: ${books.error}` );
+                    }
+
+                    // Validate response against book shelf data.
+                    return this.validateBooks(books);
+                })
+                .then(validated => {
+                    this.setState({
+                        books: validated,
+                        error: false
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.setState({error: true});
+                })
+        , 500);
+    }
+
+    handleAutoSuggest = (e) => {
+        const value = e.target.textContent;
+        this.updateInput(value);
+        this.updateResults(value);
+        this.setState({
+            query: value,
+            suggestions: []
+        });
+    }
+
+    updateInput = (val) => this.searchInput.value = val;
+
     render() {
             /**
              * Discrepency in search results here. The first call returns
              * a shelf property of "wantToRead" for the book id of the second call.
-             * The second call returns a shelf property of "none".
+             * The second call returns a shelf property of "none". I sanatize the
+             * results to the bookshelf, or .getAll(), method.
              */
             // api.search('finance').then(res => console.log(res));
             // api.get('74XNzF_al3MC').then(res => console.log(res));
@@ -98,6 +129,15 @@ class Search extends Component {
                         value={this.state.query}
                         onChange={this.handleChange}
                         ref={input => { this.searchInput = input }} />
+                    <div className="search-wrap__query__suggestions">
+                        <ul>
+                            {this.state.suggestions && (
+                                this.state.suggestions.map(sugg => (
+                                    <li onClick={this.handleAutoSuggest} key={sugg}>{sugg}</li>
+                                ))
+                            )}
+                        </ul>
+                    </div>
                 </div>
                 <div className="wrap">
                     {this.state.query === '' && (
